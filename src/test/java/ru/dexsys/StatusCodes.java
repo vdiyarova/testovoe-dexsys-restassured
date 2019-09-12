1fa40f0a8e7609498bb4b5ead7a929ea1cbfa075
vdiyarova <diyarova.veronika123@gmail.com>
2019-09-12 11:52:55 +0400

    добавлен файл gitignore

--- src/test/java/ru/dexsys/StatusCodes.java
+++ src/test/java/ru/dexsys/StatusCodes.java
@@ -1,13 +1,21 @@
 package ru.dexsys;
 
+import io.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.when;
 
 public class StatusCodes {
+    @Before
+    public void setUp(){
+        RestAssured.baseURI = "http://httpbin.org/status";
+    }
+
     @Test
-    public void testCode200(){
-        when().post("http://httpbin.org/status/200")
+    public void testPostCode200(){
+        when().post("200")
                 .then().statusCode(200);
     }
 }
